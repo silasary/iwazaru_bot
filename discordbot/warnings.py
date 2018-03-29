853d24d2f5041c62159e48862ae9fd68ca2a3896
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-03-29 19:17:45 +1100

    Python is weird and bad.

--- discordbot/warnings.py
+++ discordbot/warnings.py
@@ -7,18 +7,18 @@ def parse_message(text: str) -> str:
     #     return 'gentle reminder: ableist language. check out some alternatives! http://www.autistichoya.com/p/ableist-words-and-terms-to-avoid.html (scroll down)'
     if 'guys' in text:
         return 'Gentle reminder: exclusionary language. Some alternatives: all, everyone, friends, folks, people'
-    if re.match(r'stupid|retarded|idiotic', text):
+    if re.search(r'stupid|retarded|idiotic', text, re.IGNORECASE):
         return 'Gentle reminder: ableist language. Some alternatives: pathetic, uninspiring, vapid, obtuse'
-    if re.match(r'crazy|insane|bonkers', text):
+    if re.search(r'crazy|insane|bonkers', text, re.IGNORECASE):
         return 'Gentle reminder: ableist language. Some alternatives: ludicrous, wild, ridiculous, absurd, chaotic, silly, nonsensical, unreal, unbelievable'
-    if re.match(r'mad', text):
+    if re.search(r'mad', text, re.IGNORECASE):
         return 'Gentle reminder: ableist language. Some alternatives: angry, furious, annoyed'
-    if re.match(r'idiot|idiotic|imbecile|moron|retard|lunatic', text):
+    if re.search(r'idiot|idiotic|imbecile|moron|retard|lunatic', text, re.IGNORECASE):
         return 'Gentle reminder: ableist language. Some alternatives: uninformed, ignorant, incorrect, wrong'
-    if re.match(r'hysterical', text):
+    if re.search(r'hysterical', text, re.IGNORECASE):
         return 'Gentle reminder: sexist language. Some alternatives: hilarious, funny'
-    if re.match(r'dumb', text):
+    if re.search(r'dumb', text, re.IGNORECASE):
         return 'Gentle reminder: ableist language. Some alternatives: silly, foolish, ignorant, uninformed, ridiculous, pathetic, absurd'
-    if re.match(r'lame', text):
+    if re.search(r'lame', text, re.IGNORECASE):
         return 'Gentle reminder: ableist language. Some alternatives: silly, foolish, ridiculous, pathetic, absurd, uncool'
     return None
