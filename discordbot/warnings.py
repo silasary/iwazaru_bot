c8855f95cb8e50d47a679e518f930f5ef568b900
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-04-02 12:28:15 +1000

    Display matching word in reminder.

--- discordbot/warnings.py
+++ discordbot/warnings.py
@@ -1,6 +1,24 @@
 import re
 
+WARNINGS = {
+    r'\b(females)\b': 'pretty objectifying, try "women" instead!',
+    r'\b(guys)\b': 'exclusionary language. Some alternatives: all, everyone, friends, folks, people',
+    r'\b(stupid|retarded|idiotic)\b': 'ableist language. Some alternatives: pathetic, uninspiring, vapid, obtuse',
+    r'\b(crazy|insane|bonkers)\b': 'ableist language. Some alternatives: ludicrous, wild, ridiculous, absurd, chaotic, silly, nonsensical, unreal, unbelievable',
+    r'\b(mad)\b': 'ableist language. Some alternatives: angry, furious, annoyed',
+    r'\b(idiot|idiotic|imbecile|moron|retard|lunatic)\b': 'ableist language. Some alternatives: uninformed, ignorant, incorrect, wrong',
+    r'\b(hysterical)\b': 'sexist language. Some alternatives: hilarious, funny',
+    r'\b(dumb)\b': 'ableist language. Some alternatives: silly, foolish, ignorant, uninformed, ridiculous, pathetic, absurd',
+    r'\b(lame)\b': 'ableist language. Some alternatives: silly, foolish, ridiculous, pathetic, absurd, uncool',
+    r'\b(cripple|crippled)\b': 'ableist language. Some alternatives: broken, not working',
+    r'\b(spaz|derp|spastic)\b': 'ableist language. check out some alternatives! <http://www.autistichoya.com/p/ableist-words-and-terms-to-avoid.html> (scroll down)',
+}
+
 def parse_message(text: str) -> str:
+    for key in WARNINGS:
+        m = re.search(key, text, re.IGNORECASE)
+        if m:
+            return 'gentle reminder: {0} is {1}'.format(m.group(0), WARNINGS[key])
     if 'females' in text:
         return 'Saying "females" is pretty objectifying, try "women" instead!'
     if re.match(r'\b(spaz|derp|spastic)\b', text):
