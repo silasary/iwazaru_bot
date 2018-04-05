028c928017b5648e585cc89c59805d798a9bdd77
Katelyn Gigante <clockwork.singularity@gmail.com>
2018-04-05 14:46:20 +1000

    Adding more slurs. :/

--- discordbot/warnings.py
+++ discordbot/warnings.py
@@ -1,18 +1,24 @@
 import re
 
 WARNINGS = {
+    # Sexist/exclusionary language
     r'\b(females)\b': 'pretty objectifying, try "women" instead!',
     r'\b(guys)\b': 'exclusionary language. Some alternatives: all, everyone, friends, folks, people',
+    r'\b(hysterical)\b': 'sexist language. Some alternatives: hilarious, funny',
+    # Mental conditions
     r'\b(stupid|retarded|idiotic)\b': 'ableist language. Some alternatives: pathetic, uninspiring, vapid, obtuse',
     r'\b(crazy|insane|bonkers)\b': 'ableist language. Some alternatives: ludicrous, wild, ridiculous, absurd, chaotic, silly, nonsensical, unreal, unbelievable',
     r'\b(mad)\b': 'ableist language. Some alternatives: angry, furious, annoyed',
     r'\b(idiot|idiotic|imbecile|moron|retard|lunatic)\b': 'ableist language. Some alternatives: uninformed, ignorant, incorrect, wrong',
-    r'\b(hysterical)\b': 'sexist language. Some alternatives: hilarious, funny',
+    # Physical conditions
     r'\b(dumb)\b': 'ableist language. Some alternatives: silly, foolish, ignorant, uninformed, ridiculous, pathetic, absurd',
     r'\b(lame)\b': 'ableist language. Some alternatives: silly, foolish, ridiculous, pathetic, absurd, uncool',
     r'\b(cripple|crippled)\b': 'ableist language. Some alternatives: broken, not working',
+    # Racism
+    r'\b(gypsy)\b': 'a racial slur against the Romani people.',
+    r'\b(nigger|nigga|negro)\b': 'a racial slur against black people.',
+    # catchall ableist (No suggestions)
     r'\b(psycho|schitzo|schizo|spaz|derp|spastic|spacker)\b': 'ableist language. check out some alternatives! <http://www.autistichoya.com/p/ableist-words-and-terms-to-avoid.html> (scroll down)',
-
 }
 
 def parse_message(text: str) -> str:
