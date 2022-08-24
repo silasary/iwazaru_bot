03febfea9dcddfc9b4aa67be262b3a8e07352277
Katelyn Gigante <clockwork.singularity@gmail.com>
2022-08-25 00:16:27 +1000

    linting

--- discordbot/messagedata.py
+++ discordbot/messagedata.py
@@ -8,5 +8,5 @@ __all__ = 'MessageData'
 
 class MessageData(Munch):
     def __init__(self) -> None:
-        self.response_message: Message = None
+        self.response_message: Optional[Message] = None
         self.response_text: Optional[str] = None
